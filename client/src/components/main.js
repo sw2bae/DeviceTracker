4fa17bf92dacbe7792983cbedf306f13f7789434
Sang Bae <sw2.bae@gmail.com>
2020-09-13 17:01:29 -0500

    rev02_boostrap Added

---
+++ client/src/components/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { Link, useLocation } from "react-router-dom";
+
+function Main() {
+    return (
+        <main className="container text-center card mt-5">
+            <h1 className="mt-3 mb-3">Main</h1>
+            <p className="mt-3">Main</p>
+        </main>
+    );
+}
+
+export default Main;
--- server.js
+++ server.js
@@ -3,17 +3,46 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const passport = require("./config/passport");
+const expressSession = require("express-session");
+const db = require('./models');
+const routes = require('./routes');
+
+app.use(expressSession({ secret: process.env.sessionSecret, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Define API routes here
+app.use(routes);
+
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+const syncOptions = { force: true };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === 'test') {
+  syncOptions.force = true;
+};
+
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(() => {
+  app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
 });
