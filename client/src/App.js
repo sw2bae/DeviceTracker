7516328470eba5dc6dccc66c770313fbf5d82afb
Sang Bae <sw2.bae@gmail.com>
2020-09-28 14:11:08 -0500

    rev29_ProtectedRoute Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
+import API from "./utils/API";
+import ProtectedRoute from './components/ProtectedRoute';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
 import Footer from "./components/footer";
-// import API from "./utils/API";
 import Main from "./pages/Main";
 import Log from "./pages/Log";
 
 class App extends Component {
-  // const [user, setUser] = useState();
+
   state = {
     currentUser: {},
     isAuthenticated: false
   };
 
-  //verify login 
-  // async componentDidMount() {
-  //   const { user } = await API.checkAuth();
-  //   if (user && user.userId) {
-  //     this.setState({
-  //       currentUser: user,
-  //       isAuthenticated: true
-  //     });
-  //   };
-  // };
+  async componentDidMount() {
+    const { user } = await API.checkAuth();
+    if (user && user.userId) {
+      this.setState({
+        currentUser: user,
+        isAuthenticated: true
+      });
+    };
+  };
 
   //protected routes check to make sure user is logged in before allowing to continue to target, otherwise redirect to main page
   render() {
@@ -32,10 +32,8 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={LogIn} />
           <Route path="/signup/" component={SignUp} />
-          <Route path="/main/" component={Main} />
-          <Route path="/log/" component={Log} />
-          {/* <ProtectedRoute exact={true} path="/main/" component={Main} isAuthenticated={this.state.isAuthenticated} />
-          <ProtectedRoute exact={true} path="/log/" component={Log} isAuthenticated={this.state.isAuthenticated} /> */}
+          <ProtectedRoute exact={true} path="/main/" component={Main} isAuthenticated={this.state.isAuthenticated} />
+          <ProtectedRoute exact={true} path="/log/" component={Log} isAuthenticated={this.state.isAuthenticated} />
         </Switch>
         <Footer />
       </Router >
