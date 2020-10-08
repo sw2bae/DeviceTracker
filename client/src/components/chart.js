71b9d92684c2060e341dd2f8f140165ec9785f80
Sang Bae <sw2.bae@gmail.com>
2020-10-07 19:42:06 -0500

    rev49_Published_ver0.8_Typo&Space Added

--- client/src/components/chart.js
+++ client/src/components/chart.js
@@ -33,11 +33,11 @@ function Chart({ model1, model2, model3, model1AgingRoom, model2AgingRoom, model
                 <h1 className="font-weight-bold text-center mt-3"> 	🎨Palette </h1>
                 <h1 className="font-weight-bold text-center mt-3 mb-5"> {sum} EA.</h1>
                 <Link type="button" className="btn btn-info mb-3" to='/O1'>O1 : {model1} ea. </Link>
-                <p className="font-weight-bold">↳ [AgingRoom : {model1AgingRoom}ea.] + [Outbound : {model1 - model1AgingRoom}ea.]</p>
+                <p className="font-weight-bold">↳ [AgingRoom : {model1AgingRoom} ea.] + [OutBound : {model1 - model1AgingRoom} ea.]</p>
                 <Link type="button" className="btn btn-info mb-3" to='/T2'>T2 : {model2} ea.</Link>
-                <p className="font-weight-bold">↳ [AgingRoom : {model2AgingRoom}ea.] + [Outbound : {model2 - model2AgingRoom}ea.]</p>
+                <p className="font-weight-bold">↳ [AgingRoom : {model2AgingRoom} ea.] + [OutBound : {model2 - model2AgingRoom} ea.]</p>
                 <Link type="button" className="btn btn-info mb-3" to='/P3'>P3 : {model3} ea.</Link>
-                <p className="font-weight-bold">↳ [AgingRoom : {model3AgingRoom}ea.] + [Outbound : {model3 - model3AgingRoom}ea.]</p>
+                <p className="font-weight-bold">↳ [AgingRoom : {model3AgingRoom} ea.] + [OutBound : {model3 - model3AgingRoom} ea.]</p>
             </div>
             <div className="col-1"></div>
             <div className="card mt-5 mb-5 col-sm">
